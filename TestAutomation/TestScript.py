51ebe29bcea66662b7eed06190ec4f5cac5957ab
Padmapriya <Padmapriyaadarsh@gmail.com>
2025-11-20 19:25:36 +0530

    Updates

--- TestAutomation/TestScript.py
+++ TestAutomation/TestScript.py
@@ -29,9 +29,9 @@ options = Options()
 # options.add_argument("--headless=new")
 # Ensure a consistent viewport for screenshots in headless
 # options.add_argument("--window-size=1920,1080")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+# options.add_argument("--disable-gpu")
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=options)
 
